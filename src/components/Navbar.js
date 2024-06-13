17284d14ff0af3c9108d2c24091b22d762e0b7c4
Tauheed <tauheeddarekar786@gmail.com>
2024-06-14 01:03:50 +0530

    Updated the UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/tauheed78600"
                 target="_blank"
                 className="fork-btn-inner"
               >
